ba2ef752c4391cda4e4cbf7e5ca3df634014cdb6
vinechild <36977619+vinechild@users.noreply.github.com>
2018-03-01 21:51:42 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,11 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'm!ping') {
     	message.channel.send("pong!");
-  	}
+  	} else
+        
+        if (message.content === 'm!foo') {
+            message.channel.send("bar!");
+        }
 });
 
 // THIS  MUST  BE  THIS  WAY
